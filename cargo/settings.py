d64ba1cff9eb5187e6ce2424a324d1186efad74b
git <git>
2022-08-08 21:32:45 +0600

    FIX DB

--- cargo/settings.py
+++ cargo/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 import dotenv
 
 
@@ -151,6 +151,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=500)
 
 # CORS config
 CORS_ALLOWED_ORIGINS = True
