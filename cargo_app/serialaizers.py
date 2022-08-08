58e5e1bc99e95268e0dc0f117e21cd4628c514fd
git <git>
2022-08-08 21:13:45 +0600

    serializers upgrate

--- cargo_app/serialaizers.py
+++ cargo_app/serialaizers.py
@@ -6,23 +6,19 @@ from .models import (Truck, Customer, Cargo,
 
 class TruckSerialaizer(serializers.ModelSerializer):
     
+    curgo_category = serializers.StringRelatedField(
+        read_only=True
+    )
+    
     class Meta:
         model = Truck
         fields = '__all__'
         
-class InvoiceSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = Invoice
-        fields = '__all__'
-        
-class OrderSerialaizer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = Order
-        fields = '__all__'
-        
 class DriverSerializer(serializers.ModelSerializer):
+    
+    curgo_category = serializers.StringRelatedField(
+        read_only=True
+    )
     
     class Meta:
         model=Driver
@@ -42,6 +38,25 @@ class CargoSerializer(serializers.ModelSerializer):
         model=Cargo
         fields = '__all__'
         
+class InvoiceSerializer(serializers.ModelSerializer):
+    
+    cargo = CargoSerializer(
+        many=True
+    )
+    point_a = serializers.StringRelatedField(
+        read_only=True
+    )
+    point_b = serializers.StringRelatedField(
+        read_only=True
+    )
+    customer = serializers.StringRelatedField(
+        read_only=True
+    )
+    
+    class Meta:
+        model = Invoice
+        fields = '__all__'
+        
 class CustomerSerializer(serializers.ModelSerializer):
     
     class Meta:
@@ -56,6 +71,26 @@ class CategorySerializer(serializers.ModelSerializer):
         
 class WayBillSerializer(serializers.ModelSerializer):
     
+    point_a = serializers.StringRelatedField(
+        read_only=True
+    )
+    point_b = serializers.StringRelatedField(
+        read_only=True
+    )
+    drivers = DriverSerializer(read_only=True)
+    customer = serializers.StringRelatedField(
+        read_only=True
+    )
+    
     class Meta:
         model=WayBill
+        fields = '__all__'
+        
+class OrderSerialaizer(serializers.ModelSerializer):
+    
+    way_bill = WayBillSerializer(read_only=True)
+    invoice = InvoiceSerializer(many=True)
+    
+    class Meta:
+        model = Order
         fields = '__all__'
